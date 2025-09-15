5489c1208ee1888045a096c94d9c41469af1b415
majiac065-ops <majiac065@gmail.com>
2025-09-15 16:07:45 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,10 @@
+s1=int(input("marks of s1:"))
+s2=int(input("marks of s2:"))
+s3=int(input("marks of s3:"))
+s4=int(input("marks of s4:"))
+s5=int(input("marks of s5:"))
+total=s1+s2+s3+s4+s5
+print("total marks of 5 subjects:",total)
+percentage=(total/1000)*100
+print("percentage of marks",percentage)
+                                                                                                                                                                       ArithmeticError     
