04b09a79964506d07ec7469d7f810566136827dd
majiac065-ops <majiac065@gmail.com>
2025-09-15 16:06:50 +0530

    Add files via upload

---
+++ firstpgm.py
@@ -0,0 +1,15 @@
+
+a=int(input("enter the length;"))
+
+b=int(input("enter the width;"))
+
+c=a*b
+print("area of rectangle",c)
+
+a=int(input("enter the first number;"))
+b=int(input("enter the secount number;"))
+c=a+b
+print("result is",c)
+
+
+
