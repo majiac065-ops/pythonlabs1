ff426cf7975d79cdbba414375994e928291556d9
majiac065-ops <majiac065@gmail.com>
2025-10-30 10:53:11 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,5 @@
+num1 = int(input("enter a first number"))
+num2 = int(input("enter a second number"))
+num3 = int(input("enter a third number"))
+largest_number = max(num1,num2,num3)
+print(f"the largers number is {largest_number}")
