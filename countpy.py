ee3bd43c9c5eedd230c84fcd2959f5dea52ff97d
majiac065-ops <majiac065@gmail.com>
2025-10-30 10:53:45 +0530

    Add files via upload

---
+++ countpy.py
@@ -0,0 +1,10 @@
+word= "mango"
+count= 0
+open("temp.txt", 'r')s f:
+    for line in f for line in f:
+    words = line.split()
+        for i in words:
+            if(i==word):
+                count=count+1
+                     print("Occurrences of the word", word, ":", count)
+    
