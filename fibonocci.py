553a43b0aed2352f45581f9802614c9f89ac4e50
majiac065-ops <majiac065@gmail.com>
2025-10-30 10:54:18 +0530

    Add files via upload

---
+++ fibonocci.py
@@ -0,0 +1,17 @@
+def fibonacci_series(n):
+    fib = [0, 1]
+    for i in range(2, n):
+    return
+
+def print_fibonacci_triangle(n):
+    for i in range(1, n + 1):
+        for j in range(i):
+            if index < len(fib):
+                print(fib[index], end=" ")
+                index += 1
+        print()
+
+# Get input from user
+n = int(input("Enter number of terms: "))
+print("\nFibonacci Series in Triangle Form:\n")
+print_fibonacci_triangle(n)
