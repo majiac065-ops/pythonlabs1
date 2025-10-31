dd537e1d76b3cad1e146ef7a1682bd96ae38893c
majiac065-ops <majiac065@gmail.com>
2025-10-31 11:08:15 +0530

    Add files via upload

---
+++ list.python.py
@@ -0,0 +1,14 @@
+list1=[1,2,3,4,5]
+list2 =[6,7,8,9,10]
+leng1=len(list1)
+leng2=len(list2)
+if(leng1=leng):
+    print("list of samme length")
+else:
+    print("list of not same length")
+sum1=sum(list1)
+sum2=sum(list2)
+if
+    print("list is same value")
+else
+    print("list is not same valu
